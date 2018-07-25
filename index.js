92c00af2b6685d66edf722c731f05e4af2681d9f
Jean <jean@data.gov.sg>
2018-07-25 10:46:56 +0800

    Support numbers

--- index.js
+++ index.js
@@ -3,6 +3,12 @@
 var legend = require("./helpers").legend;
 var search = require("./helpers").search;
 var reverse = require("./helpers").reverse;
+var len = legend.length-1;
+
+function mod(n, p) 
+{
+    return n - p * Math.floor( n / p );
+}
 
 exports.encrypt = function(message, key) {
   // start empty string to add message to
@@ -16,17 +22,15 @@ exports.encrypt = function(message, key) {
     var valueString = search(message[i], legend);
     // numeric value for key letters
     var valueKey = search(key[i], legend);
+    var intString = typeof valueString === 'undefined' ? parseInt(message[i]) : valueString
+    var intKey = typeof valueKey === 'undefined' ? parseInt(key[i]) : valueKey
     // if the key isn't long enough, append the letter in the same
     // position as the message to the string
-    if (typeof valueString === "undefined" || typeof valueKey === "undefined") {
+    if(isNaN(intString) || isNaN(intKey)){
       encryptedMessage += message[i]
     } else {
       // add value of message to value of key minus 1
-      var reverseValue = valueString + valueKey - 1;
-      if (reverseValue > 26) {
-        // start back over to A if it gets past Z
-        reverseValue = reverseValue - 26;
-      }
+      var reverseValue = mod(intString + intKey, len);
       // convert new numeric values back to letters
       var reverseLookup = reverse(reverseValue, legend);
       // append them to the output
@@ -43,13 +47,12 @@ exports.decrypt = function(message, key) {
   for (var i = 0; i < message.length; i++) {
     var valueString = search(message[i], legend);
     var valueKey = search(key[i], legend);
-    if (typeof valueString === "undefined" || typeof valueKey === "undefined") {
+    var intString = typeof valueString === 'undefined' ? parseInt(message[i])  : valueString
+    var intKey = typeof valueKey === 'undefined' ? parseInt(key[i]) : valueKey
+    if(isNaN(intString) || isNaN(intKey)){
       encryptedMessage += message[i]
     } else {
-      var reverseValue = valueString - valueKey + 1;
-      if (reverseValue < 0) {
-        reverseValue = reverseValue + 26;
-      }
+      var reverseValue = mod(intString - intKey, len);
       var reverseLookup = reverse(reverseValue, legend);
       encryptedMessage += reverseLookup;
     }
